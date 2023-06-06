2bb4542fb959b77f1785df41a91085c904a3d28f
JongKown Jo <41224905+Jongkwon-jo@users.noreply.github.com>
2023-06-06 14:45:46 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,17 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+st.title('Seoul House Price')
+st.header('Seoul House Price Prediction Project')
+
+
+# 지역 변수를 제외한 나머지 변수 설정하는 sidebar
+width = st.slider("면적을 선택하세요 단위(m^2)", 11, 291)
+# 6. 날짜 입력
+contractdate = st.date_input('계약년월')  # 디폴트로 오늘 날짜가 찍혀 있다.
+contractString = contractdate.strftime("%Y%m")
+contract = float(contractString)
+st.write(contractdate.strftime("%Y%m"))
+
+floor = st.slider("층을 선택하세요", 1, 60)
+builtYear = st.slider("건축년도를 선택하세요(년도)", 1971, 2022)
