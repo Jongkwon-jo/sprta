0a11ee58937402250b1ae3a5b356ffbb4c6e6609
JongKown Jo <41224905+Jongkwon-jo@users.noreply.github.com>
2023-06-06 15:26:34 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import time
+import torch
+
+model = torch.load('./model.pt')
 
 st.title('Seoul House Price')
 st.header('Seoul House Price Prediction Project')
