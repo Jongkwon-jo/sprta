c9c221ca903eb9d921ad393e8c12ba645ece8acf
JongKown Jo <41224905+Jongkwon-jo@users.noreply.github.com>
2023-06-06 14:48:01 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,29 @@ import numpy as np
 st.title('Seoul House Price')
 st.header('Seoul House Price Prediction Project')
 
+# Add a placeholder 진행 상황 바
+latest_iteration = st.empty()
+bar = st.progress(0)
+for i in range(100):
+    # Update the progress bar with each iteration.
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.01)
+    
+# 변수는 총 11개
+# 순서대로 전용면적, 계약년월, 거래금액, 층, 건축년도
+st.write("""
+## 사용 변수들
+1. 전용면적
+2. 계약년월
+3. 층
+4. 건축년도
+""")
+
+# 지역을 고르는 select box
+option = st.sidebar.selectbox(
+    '어떤 지역을 고르시겠습니까?',
+    ('용현동', '구월동', '송도동', '주안동', "숭의동", "연수동", "부평동", "청라동", "동춘동", "학익동"))
 
 # 지역 변수를 제외한 나머지 변수 설정하는 sidebar
 width = st.slider("면적을 선택하세요 단위(m^2)", 11, 291)
